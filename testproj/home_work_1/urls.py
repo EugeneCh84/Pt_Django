4852f8e374904a1e2aff8b47228d6fe44ff7976c
eugene <eugene.cherepanov.84@gmail.com>
2023-10-01 20:14:08 +0500

    add logging

--- testproj/home_work_1/urls.py
+++ testproj/home_work_1/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('main/', views.main, name='Main page'),
-    path('about/', views.about, name='About me'),
+    path('about/', views.about, name='About myself'),
 ]
--- testproj/home_work_1/views.py
+++ testproj/home_work_1/views.py
@@ -7,53 +7,51 @@ from django.http import HttpResponse
 # from django.shortcuts import render
 
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('my_logger')
 
-headers = {'Cache-Control': 'no-cache, must-revalidate',
-           'Pragma': 'no-cache'}
-
+headers = {'Cache-Control': 'no-cache, must-revalidate', 'Pragma': 'no-cache'}
 
 def main(request):
     body = """
-    <title>Главная страница</title>
+    <title>Main page</title>
     <body>
         <div>
-            <h1>Главная страница</h1>
-            <p>Содержимое главной страницы</p>
-            <p>Перейдите на страницу: /about_me</p>
+            <h1>Main page</h1>
+            <p>Content of main page</p>
+            <p>Go to page: /about</p>
         </div>
         <footer>
             <div>
                 <p>Copyright &copy;
                     <script type="text/javascript"> document.write(new Date().getFullYear());</script>
-                    Communications Inc. Все права защищены.
+                    
                 </p>
             </div>
         </footer>
     </body>
     """
-    logger.info(f'Страница открыта: {body}')
+    logger.info(f'page open: main')
     return HttpResponse(body, charset="utf-8", headers=headers)
 
 
 def about(request):
     body = """
-        <title>О себе</title>  
+        <title>About myself</title>  
         <body>     
             <div>
-                <h1>Валетов Андрей Васильевич</h1>
-                <p>Мужчина, 50 лет, родился 30 октября 1972</p>
-                <p>Перейдите на страницу: /main</p>
+                <h1>Cherepanov Evgeniy Aleksandrovich</h1>
+                <p>Male, 38 years old, was born 24 of october 1984</p>
+                <p>Go to page: /main</p>
             </div>
             <footer>
                 <div>
                     <p>Copyright &copy;
                         <script type="text/javascript"> document.write(new Date().getFullYear());</script>
-                        Communications Inc. Все права защищены.
+                        
                     </p>
                 </div>
             </footer>
         </body>
         """
-    logger.info(f'Страница открыта: {body}')
+    logger.info(f'page open: about')
     return HttpResponse(body, charset="utf-8", headers=headers)
